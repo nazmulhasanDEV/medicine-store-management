a9669a53ee3404e048bd5873707fa82a99f354ca
nazmulhasan7447 <nazmulhasan747406@gmail.com>
2023-08-18 14:39:56 +0600

    added requirements.txt file

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include, re_path
 import uuid
+from . import views
 
 urlpatterns = [
-
+    re_path(r'^index$/', views.index, name="index"),
 ]
--- core/views.py
+++ core/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+
+def index(request):
+
+    return render(request, 'index.html')
